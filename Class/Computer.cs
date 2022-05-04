55fb06e7b65294d4fe6c8da0d11b1d7a40b75e3f
Ers <Ers@DESKTOP-SCCLTAH>
2022-05-04 14:38:06 +0300

    -

---
+++ Class/Computer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Computer
+    {
+        //OOP gives us freedom to create objects inside application freely. To create and use an object we must create a prototype first. We can create as many as we want and where we need. We hold all those objects inside of a class. 
+        
+        //Default access modifier is private, to be able to reach and use the properties we need to mark them as public.
+        string CPU;
+        private string DisplayCard; 
+
+       
+        public int MemoryCapacity;
+        public string MemoryType;
+        public bool DisplaySecreen;
+    }
+}
---
+++ Class/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Creating instance of a new Computer named hp. So we can reach properties of a class after creating it as an object and assign values to them. 
+            Computer hp = new Computer();
+
+            Computer samsung = new Computer(); // we can create as many as we want...
+
+            hp.DisplaySecreen = true; // we only reach public properties.
+            hp.MemoryType = "DDR5";
+            hp.MemoryCapacity = 32;
+
+            MessageBox.Show("Display: " + hp.DisplaySecreen + "\n" + "Memory Type: " + hp.MemoryType + "\n" + "Memory Capacity: " + hp.MemoryCapacity);
+        }
+    }
+}
